f84503bff924def5693fff6342e1e1b3cce9c2b3
GenericOctopus <genericoctopus@gmail.com>
2022-12-16 22:33:52 -0500

    exercise 2 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,18 @@
-const repeatString = function() {
+const repeatString = function(inputString, inputNumber) {
+
+    var repeatString = '';
+
+    if (inputNumber < 0){
+        return "ERROR";
+    } 
+
+
+    for (let index = 0; index < inputNumber; index++) {
+        
+        repeatString = repeatString + inputString
+    }
+
+    return repeatString;
 
 };
 
